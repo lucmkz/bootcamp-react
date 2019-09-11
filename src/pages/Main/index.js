5356d8912ab8d42d28b433b56b96d0cc9cede5d5
Lucas <l.duarte.mk@gmail.com>
2019-09-11 09:50:35 -0300

    global styles

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,10 +3,5 @@ import React from 'react';
 import { Title } from './styles';
 
 export default function Main() {
-  return (
-    <Title error={false}>
-      Main
-      <small>oi</small>
-    </Title>
-  );
+  return <Title>Hello World</Title>;
 }
